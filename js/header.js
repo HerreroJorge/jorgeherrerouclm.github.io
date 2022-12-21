cec6a27bf29f95e25363ea615472ac706bde6f9d
JorgeHerreroUclm <Jorge.Herrero1@alu.uclm.es>
2022-12-21 17:49:42 +0100

    .

--- js/header.js
+++ js/header.js
@@ -6,44 +6,45 @@ var ul = document.getElementById('ulNombre');
 var boton = document.getElementById('buttonCV');
 
 var heightHeader = header.offsetTop;
-window.addEventListener('scroll', function () {
+
+document.addEventListener('scroll', function () {
     'use strict';
-    if ((window.pageYOffset > 1 && window.pageYOffset < 899)){
+    if ((window.scrollY > 1 && window.scrollY < 899)){
         header.classList.add('fixed');
     } else{
         header.classList.remove('fixed');
     }
-    if (window.pageYOffset > 899 && window.pageYOffset < 2200){
+    if (window.scrollY >= 899 && window.scrollY < 2200){
         header.classList.add('fixed2');
     } else{
         header.classList.remove('fixed2');
     }
-    if ((window.pageYOffset > 2200)){
+    if ((window.scrollY > 2200)){
         header.classList.add('fixed3');
     } else{
         header.classList.remove('fixed3');
     }
-    if (window.pageYOffset > 530 && window.pageYOffset < 850){
+    if (window.scrollY > 530 && window.scrollY < 850){
         nombre.classList.add('h1NombreFixed');
     } else{
         nombre.classList.remove('h1NombreFixed');
     }
-    if (window.pageYOffset > 570 && window.pageYOffset < 890){
+    if (window.scrollY > 570 && window.scrollY < 890){
         row.classList.add('rowNombreFixed');
     } else{
         row.classList.remove('rowNombreFixed');
     }
-    if (window.pageYOffset > 610 && window.pageYOffset < 930){
+    if (window.scrollY > 610 && window.scrollY < 930){
         p.classList.add('pNombreFixed');
     } else{
         p.classList.remove('pNombreFixed');
     }
-    if (window.pageYOffset > 650 && window.pageYOffset < 970){
+    if (window.scrollY > 650 && window.scrollY < 970){
         ul.classList.add('ulNombreFixed');
     } else{
         ul.classList.remove('ulNombreFixed');
     }
-    if (window.pageYOffset > 690 && window.pageYOffset < 1010){
+    if (window.scrollY > 690 && window.scrollY < 1010){
         boton.classList.add('buttonCVFixed');
     } else{
         boton.classList.remove('buttonCVFixed');
